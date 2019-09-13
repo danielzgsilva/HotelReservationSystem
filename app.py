8077df448760c2df485b96b1c174ff458683bab0
Daniel Silva <danielzgsilva@knights.ucf.edu>
2019-09-13 00:01:52 -0400

    Home Page

--- app.py
+++ app.py
@@ -5,20 +5,22 @@ app = Flask(__name__)
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
-
 @app.route("/")
 @app.route("/home")
 def index():
     return render_template('index.html')
+
+@app.route('/<filename>')
+def load_image(filename):
+    return send_from_directory('static/', filename)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def InternalServerError(e):
-    home_files, face_files, text, confs = FaceClassification.generate_home()
-    return render_template('index.html', title='Home', examples=home_files, face_files=face_files, text=text), 500
+def internal_server_error(e):
+    return render_template('index.html'), 500
 
 if __name__ == "__main__":
     app.run('localhost', port=5555)
